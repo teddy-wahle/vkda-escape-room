d60617ed40612c0a2e71f95ceb6ad9d7a54752bc
Teddy Wahle <teddy.wahle@verkada.com>
2022-04-06 16:38:10 -0700

    Flask app set up

---
+++ app.py
@@ -0,0 +1,11 @@
+# A welcome message to test our server
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
