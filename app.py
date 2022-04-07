da17473842964d7867acd8cd41c9a5aa44c07ea1
Naresh <rnaresh.n@gmail.com>
2022-04-07 14:33:42 -0400

    Add mechanism to not let users start multiple games.
    Signed-off-by: Naresh <rnaresh.n@gmail.com>

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from escape_room import EscapeRoom
 
 logging.basicConfig(level=logging.DEBUG, format='%(name)s - %(levelname)s - %(message)s')
 app = Flask(__name__)
-game = EscapeRoom()
+game = None
 
 
 @app.route("/ping")
@@ -18,6 +18,9 @@ def index():
 @app.route("/start")
 def start():
     global game
+    if game:
+        return jsonify({"message": "stop previous game before starting a new one"}), 400
+
     game = EscapeRoom()
     game.start()
     return jsonify({}), 200
@@ -25,17 +28,25 @@ def start():
 
 @app.route("/current")
 def current():
+    if not game:
+        return jsonify({"message": "game not started"}), 400
     return jsonify({"current": game.current}), 200
 
 
 @app.route("/points")
 def points():
+    if not game:
+        return jsonify({"message": "game not started"}), 400
     return jsonify({"points": game.points}), 200
 
 
 @app.route("/stop")
 def stop():
+    global game
+    if not game:
+        return jsonify({"message": "game not started"}), 400
     game.stop()
+    game = None
     return jsonify({}), 200
 
 
